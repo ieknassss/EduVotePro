42a3c0e8facc2b548bd2eccc33bdcb6a70bae551
Iekna Lora <ieknalora@gmail.com>
2026-05-09 23:05:47 -0400

    Implementacion del Dashboard y menu

---
+++ Presentation/Forms/FrmDashboard.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace ProyectoSistemaEletoralEstudiantil.Presentation.Forms
+{
+    partial class FrmDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblRole = new System.Windows.Forms.Label();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.btnElections = new System.Windows.Forms.Button();
+            this.btnUsers = new System.Windows.Forms.Button();
+            this.btnParties = new System.Windows.Forms.Button();
+            this.btnReports = new System.Windows.Forms.Button();
+            this.btnVote = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelMenu.SuspendLayout();
+            this.panelTop.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.panelMenu.Controls.Add(this.btnLogout);
+            this.panelMenu.Controls.Add(this.btnVote);
+            this.panelMenu.Controls.Add(this.btnReports);
+            this.panelMenu.Controls.Add(this.btnParties);
+            this.panelMenu.Controls.Add(this.btnUsers);
+            this.panelMenu.Controls.Add(this.btnElections);
+            this.panelMenu.Controls.Add(this.btnDashboard);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(250, 450);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.panelTop.Controls.Add(this.lblRole);
+            this.panelTop.Controls.Add(this.lblUser);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(250, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(550, 70);
+            this.panelTop.TabIndex = 1;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Segoe UI Black", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblUser.Location = new System.Drawing.Point(156, 9);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(125, 38);
+            this.lblUser.TabIndex = 0;
+            this.lblUser.Text = "Usuario";
+            // 
+            // lblRole
+            // 
+            this.lblRole.AutoSize = true;
+            this.lblRole.Font = new System.Drawing.Font("Segoe UI Black", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRole.ForeColor = System.Drawing.Color.Gainsboro;
+            this.lblRole.Location = new System.Drawing.Point(324, 9);
+            this.lblRole.Name = "lblRole";
+            this.lblRole.Size = new System.Drawing.Size(63, 38);
+            this.lblRole.TabIndex = 1;
+            this.lblRole.Text = "Rol";
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.ForeColor = System.Drawing.Color.White;
+            this.btnDashboard.Location = new System.Drawing.Point(0, 0);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(250, 50);
+            this.btnDashboard.TabIndex = 0;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.UseVisualStyleBackColor = true;
+            // 
+            // btnElections
+            // 
+            this.btnElections.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnElections.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnElections.ForeColor = System.Drawing.Color.White;
+            this.btnElections.Location = new System.Drawing.Point(0, 50);
+            this.btnElections.Name = "btnElections";
+            this.btnElections.Size = new System.Drawing.Size(250, 47);
+            this.btnElections.TabIndex = 1;
+            this.btnElections.Text = "Elecciones";
+            this.btnElections.UseVisualStyleBackColor = true;
+            // 
+            // btnUsers
+            // 
+            this.btnUsers.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnUsers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUsers.ForeColor = System.Drawing.Color.White;
+            this.btnUsers.Location = new System.Drawing.Point(0, 97);
+            this.btnUsers.Name = "btnUsers";
+            this.btnUsers.Size = new System.Drawing.Size(250, 47);
+            this.btnUsers.TabIndex = 2;
+            this.btnUsers.Text = "Usuarios";
+            this.btnUsers.UseVisualStyleBackColor = true;
+            // 
+            // btnParties
+            // 
+            this.btnParties.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnParties.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnParties.ForeColor = System.Drawing.Color.White;
+            this.btnParties.Location = new System.Drawing.Point(0, 144);
+            this.btnParties.Name = "btnParties";
+            this.btnParties.Size = new System.Drawing.Size(250, 47);
+            this.btnParties.TabIndex = 3;
+            this.btnParties.Text = "Planchas";
+            this.btnParties.UseVisualStyleBackColor = true;
+            // 
+            // btnReports
+            // 
+            this.btnReports.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReports.ForeColor = System.Drawing.Color.White;
+            this.btnReports.Location = new System.Drawing.Point(0, 191);
+            this.btnReports.Name = "btnReports";
+            this.btnReports.Size = new System.Drawing.Size(250, 47);
+            this.btnReports.TabIndex = 4;
+            this.btnReports.Text = "Reportes";
+            this.btnReports.UseVisualStyleBackColor = true;
+            // 
+            // btnVote
+            // 
+            this.btnVote.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnVote.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVote.ForeColor = System.Drawing.Color.White;
+            this.btnVote.Location = new System.Drawing.Point(0, 238);
+            this.btnVote.Name = "btnVote";
+            this.btnVote.Size = new System.Drawing.Size(250, 47);
+            this.btnVote.TabIndex = 5;
+            this.btnVote.Text = "Votar";
+            this.btnVote.UseVisualStyleBackColor = true;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.ForeColor = System.Drawing.Color.White;
+            this.btnLogout.Location = new System.Drawing.Point(0, 285);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(250, 47);
+            this.btnLogout.TabIndex = 6;
+            this.btnLogout.Text = "Cerrar Sesión";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(291, 90);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 120);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.panel2.Location = new System.Drawing.Point(553, 285);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(220, 120);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.panel3.Location = new System.Drawing.Point(553, 98);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(220, 120);
+            this.panel3.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.panel4.Location = new System.Drawing.Point(291, 285);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(220, 120);
+            this.panel4.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Elephant", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(60, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "125";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Elephant", 9.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(321, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Votos Emitidos";
+            // 
+            // FrmDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelTop);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "FrmDashboard";
+            this.Text = "Dashboard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FrmDashboard_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Button btnReports;
+        private System.Windows.Forms.Button btnParties;
+        private System.Windows.Forms.Button btnUsers;
+        private System.Windows.Forms.Button btnElections;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Label lblRole;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnVote;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Presentation/Forms/FrmDashboard.cs
@@ -0,0 +1,74 @@
+﻿using ProyectoSistemaEletoralEstudiantil.DataAccess.Models;
+using ProyectoSistemaEletoralEstudiantil.Security.Sessions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoSistemaEletoralEstudiantil.Presentation.Forms
+{
+    public partial class FrmDashboard : Form
+    {
+        EduVoteProEntities db =
+    new EduVoteProEntities();
+        public FrmDashboard()
+        {
+            InitializeComponent();
+
+            lblUser.Text = UserSession.Nombre;
+
+            lblRole.Text = UserSession.Role;
+
+            LoadPermissions();
+            LoadStatistics();
+        }
+
+        private void LoadPermissions()
+        {
+            if (UserSession.Role == "Estudiante")
+            {
+                btnUsers.Visible = false;
+
+                btnParties.Visible = false;
+
+                btnReports.Visible = false;
+
+                btnElections.Visible = false;
+            }
+
+            if (UserSession.Role == "Administrador Partido")
+            {
+                btnUsers.Visible = false;
+            }
+        }
+
+        private void LoadStatistics()
+        {
+            int users = db.Users.Count();
+
+            int votes = db.Votes.Count();
+
+            int elections = db.Elections.Count();
+
+            MessageBox.Show(
+                "Usuarios: " + users +
+                "\nVotos: " + votes +
+                "\nElecciones: " + elections
+            );
+        }
+        private void FrmDashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
